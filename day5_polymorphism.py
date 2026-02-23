cfaad3b58ae519e0a1a6dd42163a886aa8311a51
nopaleoon <ghaninaufal2006@gmail.com>
2026-02-23 21:08:13 +0700

    Day 5 : Polymorphism + contoh  program sederhana

--- day5_polymorphism.py
+++ day5_polymorphism.py
@@ -16,7 +16,30 @@ anjing1 = Anjing()
 anjing1.suara()
 
 
-# hewan_list = [Kucing(), Anjing()]
 
-# for h in hewan_list:
-#     h.suara()
+hewan_list = [Kucing(), Anjing()]
+
+for h in hewan_list:
+    h.suara()
+    
+    
+#Latihan 
+# buat 3 class berbeda
+# Mahasiswa, Dosen, Staff
+# buat satu method yg sama tetapi isinya berbeda
+
+class Mahasiswa:
+    def kerja(self):
+        print('Tugas Mahasiswa adalah belajar')
+    
+class Dosen:
+    def kerja(self):
+        print('Tugas Dosen adalah mengajar mata kuliah')
+        
+class Staff:
+    def kerja(self):
+        print('Tugas staff adalah mengelola admisnistrasi')
+        
+job = [Mahasiswa(), Dosen(), Staff()]
+for j in job:
+    j.kerja()
